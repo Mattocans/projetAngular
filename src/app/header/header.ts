d2299dcef9b120ffcb75d6f1b5dc3d81f4eae1c4
Mattocans <matthias.tocanier@gmail.com>
2025-12-16 12:37:14 +0100

    Ajout header

---
+++ src/app/header/header.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+/**
+ * @title Toolbar overview
+ */
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.html',
+  styleUrl: './header.css',
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule],
+  standalone: true
+})
+export class Header {}
